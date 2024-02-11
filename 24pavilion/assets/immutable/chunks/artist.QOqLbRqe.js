7084f2d0c60980e3eb2ae8f10d5dc0d475ede847
Seunghyuk Choi <noaidi@gmail.com>
2024-02-11 13:50:05 +0900

    ...

---
+++ 24pavilion/assets/immutable/chunks/artist.QOqLbRqe.js
@@ -0,0 +1,28 @@
+import{s as Q,e as G,c as X}from"./scheduler.WyyjKgw5.js";import{S as Y,i as Z,m as ee,s as k,e as a,n as te,f as C,c as s,a as f,q as P,d as l,r,u as W,o as ae,g as z,h as e,k as se,l as re,p as ie}from"./index.tDunVc_9.js";import{P as ne}from"./PageTitle.82zCrM00.js";import{b as oe}from"./route.D44SLDCJ.js";function le(A){let i,v,n,o,h,b,q=`<strong>ARTIST</strong><br/>
+				Born in 1967, Seoul. KOO JEONG A is internationally acclaimed for
+				her poetic interventions into pre-existing spaces, which were once
+				described “signposts to the inaccessible” that make the space “soul”
+				visible. In her practice, architectural elements such as walls, doorsills,
+				and windows are combined with texts, drawings, paintings, sculptures,
+				films, and installations, also scent, temperature, and sound like
+				invisible elements plays significant role in many her works.<br/> <br/>
+				To KOO, art functions as a transformer that constantly incites the
+				surprise of discovery, audience involvement, extending towards an
+				unstoppable state of dynamism that is always oscillating, trembling,
+				and vibrating. KOO JEONG A creates and masters both small intimate
+				experiences and large- scale impressive works – like the Glow-in-the-dark
+				Skatepark series – but her minimal and conceptual approach is always
+				elegant, engaging, esthetically pleasing and most importantly it
+				is thought provoking. Through the installations she engages the audience,
+				challenges us, and triggers our senses in multiple ways like no other
+				artist.<br/> <br/> <a href="https://www.koojeonga.com/" class="weak" target="_blank">→ WEBSITE</a>`,V,E,m,c,D,B,_,H="Courtesy of PKM Gallery. Photo by Kim Je Won",L,p,w,F=`<strong>ARTISTIC DIRECTORS</strong><br/> <em>Jacob Fabricius</em> (b.1970, Denmark) is the director of
+				Art Hub Copenhagen in Denmark. Prior to assuming this position,
+				Fabricius was the artistic director at Kunsthal Aarhus (2016-21)
+				and Busan Biennale 2020. In addition, he served as the director
+				at Kunsthal Charlottenborg, Copenhagen (2013-14), the artistic
+				director for Malmö Konsthall, Malmö (2008-12).<br/> <br/> <em>Seolhui Lee</em> (b.1987, South Korea) is a chief curator at
+				Kunsthal Aarhus in Denmark. She previously headed the Busan Biennale’s
+				exhibition team 2020. Lee served as a curator for Seoul Museum of
+				Art, Seoul (2018-19), an adjunct professor at Korea National University
+				of Arts, Seoul (2019-22), and has furthermore worked at the National
+				Museum of Modern and Contemporary Art (2012-13).`,N,T,g,u,S,R,y,J="Photo by Chae Daehan.",I;return i=new ne({props:{title:"Artist · Artistic Directors"}}),{c(){ee(i.$$.fragment),v=k(),n=a("div"),o=a("div"),h=a("div"),b=a("div"),b.innerHTML=q,V=k(),E=a("div"),m=a("figure"),c=a("img"),B=k(),_=a("caption"),_.textContent=H,L=k(),p=a("div"),w=a("div"),w.innerHTML=F,N=k(),T=a("div"),g=a("figure"),u=a("img"),R=k(),y=a("caption"),y.textContent=J,this.h()},l(t){te(i.$$.fragment,t),v=C(t),n=s(t,"DIV",{class:!0});var d=f(n);o=s(d,"DIV",{class:!0});var x=f(o);h=s(x,"DIV",{class:!0});var M=f(h);b=s(M,"DIV",{"data-svelte-h":!0}),P(b)!=="svelte-16g38qo"&&(b.innerHTML=q),V=C(M),E=s(M,"DIV",{});var j=f(E);m=s(j,"FIGURE",{class:!0});var K=f(m);c=s(K,"IMG",{src:!0,alt:!0,style:!0}),B=C(K),_=s(K,"CAPTION",{"data-svelte-h":!0}),P(_)!=="svelte-1ch1ukl"&&(_.textContent=H),K.forEach(l),j.forEach(l),M.forEach(l),L=C(x),p=s(x,"DIV",{class:!0});var O=f(p);w=s(O,"DIV",{"data-svelte-h":!0}),P(w)!=="svelte-rtb8ee"&&(w.innerHTML=F),N=C(O),T=s(O,"DIV",{});var U=f(T);g=s(U,"FIGURE",{class:!0});var $=f(g);u=s($,"IMG",{src:!0,alt:!0,style:!0}),R=C($),y=s($,"CAPTION",{"data-svelte-h":!0}),P(y)!=="svelte-qjl03v"&&(y.textContent=J),$.forEach(l),U.forEach(l),O.forEach(l),x.forEach(l),d.forEach(l),this.h()},h(){G(c.src,D=A[0]+"artist/artist.webp")||r(c,"src",D),r(c,"alt","Artist"),W(c,"aspect-ratio","343.7 / 389"),r(m,"class","image-with-caption"),r(h,"class","grid-2cols"),G(u.src,S=A[0]+"artist/artistic-directors.webp")||r(u,"src",S),r(u,"alt","Artistic directors"),W(u,"aspect-ratio","584 / 389"),r(g,"class","image-with-caption"),r(p,"class","grid-2cols"),r(o,"class","grid-sections"),r(n,"class","container")},m(t,d){ae(i,t,d),z(t,v,d),z(t,n,d),e(n,o),e(o,h),e(h,b),e(h,V),e(h,E),e(E,m),e(m,c),e(m,B),e(m,_),e(o,L),e(o,p),e(p,w),e(p,N),e(p,T),e(T,g),e(g,u),e(g,R),e(g,y),I=!0},p(t,[d]){(!I||d&1&&!G(c.src,D=t[0]+"artist/artist.webp"))&&r(c,"src",D),(!I||d&1&&!G(u.src,S=t[0]+"artist/artistic-directors.webp"))&&r(u,"src",S)},i(t){I||(se(i.$$.fragment,t),I=!0)},o(t){re(i.$$.fragment,t),I=!1},d(t){t&&(l(v),l(n)),ie(i,t)}}}function ce(A,i,v){let n;return X(A,oe,o=>v(0,n=o)),[n]}class pe extends Y{constructor(i){super(),Z(this,i,ce,le,Q,{})}}export{pe as A};
---
+++ 24pavilion/assets/immutable/entry/start.ehjKSzMq.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.SzRmFhWD.js";export{t as start};
